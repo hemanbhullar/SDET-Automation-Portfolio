77605df1cf7794585b611ecc6776ecac5936b82a
Heman Bhullar <hemanbhullar@192.168.1.34>
2025-07-29 20:15:53 +0530

    W1D3: Demostrated Polymorphism with method overriding

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main{
     public static void main(String[] args) {
         Motorcycle mybike = new Motorcycle();
+        Vehicle newVehicle = new Vehicle();
         mybike.honk();
+        newVehicle.honk();
         mybike.doWheelie();
         System.out.println("The brand is"+ mybike.brand);
     }
--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -1,5 +1,10 @@
 public class Motorcycle extends Vehicle {
 
+    @Override
+    public void honk() {
+        System.out.println("ping ping!");
+    }
+
     public void doWheelie() {
         System.out.println("Doing a cool wheelie!");
     }
